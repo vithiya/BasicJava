0d0b3bbaf800e5d3fb45688f6604c112d779d8b0
vithiya <i.vithiya@gmail.com>
2020-07-22 12:08:35 +0530

    initial commit

---
+++ Welcome.java
@@ -0,0 +1,9 @@
+
+
+
+public class Welcome {
+    public static void main(String[] args){
+       System.out.println("Welcome to my GitHub..!") ;
+    }//End of main method
+}//End of Welcome class
+//File name and public class name should be same always
